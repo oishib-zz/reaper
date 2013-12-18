4b59b13d8503b8f82262597e3cbca1ed0e5b3179
Oishi <oishib@users.noreply.github.com>
2013-12-18 12:00:34 -0500

    Oops

--- src/PlayerThread.java
+++ src/PlayerThread.java
@@ -1,107 +1,52 @@
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.event.KeyEvent;
-import java.awt.event.KeyListener;
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
-import java.net.UnknownHostException;
 
-import javax.swing.JButton;
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-import javax.swing.JTextField;
 
-public class GameClient implements ActionListener{
-    boolean playing = true;
-    private Socket socket;
-    private PrintWriter out;
-    private BufferedReader in;
-    private JFrame gameFrame;
-    private JPanel gamePanel;
-    private JTextField jackpot;
-    private JTextField points;
-    private JButton reapButton;
-    public GameClient(){
-   	 //will be switched to false when the user hits exit
-   	 
-   			 //creates socket that connects to the server at 29999
-   			 try {
-   				 socket = new Socket("localhost", 9001);
-   			 } catch (UnknownHostException e) {} catch (IOException e) {}
-   			 //out allows the client to write messages that the server will receive
-   			 try {
-   				 out = new PrintWriter(socket.getOutputStream(), true);
-   			 } catch (IOException e) {}
-   			 //in allows the client to read messages from the server
-   			 try {
-   				 in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-   			 } catch (IOException e) {}
-
-   			 //set up JFrame
-   			 gameFrame = new JFrame("reaper");
-   			 gamePanel = new JPanel();
-   			 gameFrame.add(gamePanel);
-   			 
-   			 jackpot = new JTextField ("current jackpot: not available                                        	");
-   			 points = new JTextField ("your points: not available                                        	");
-   			 reapButton = new JButton ("reap");
-   			 reapButton.addActionListener(this);
-   			 gamePanel.add (jackpot);
-   			 gamePanel.add (reapButton);
-   			 gamePanel.add (points);
-   			 
-   			 gameFrame.add(gamePanel);
-   			 
-   			 gameFrame.setSize(500, 200);
-   			 gameFrame.setVisible(true);
-   			 this.update();
+public class PlayerThread extends Thread{
+  private Socket client;
+  private PrintWriter out;
+  private BufferedReader in;
+  private static GameThread game;
+  private int id;
+  
+  public PlayerThread (Socket client, int id){
+    this.client=client;
+    try {
+      this.out = new PrintWriter(client.getOutputStream(), true);
+      this.in = new BufferedReader(new InputStreamReader(client.getInputStream()));
+      this.id = id;
     }
-    public static void main(String[] args) throws IOException
-    {
-   	 GameClient gameC = new GameClient();
-   	 while (true){
-   		 gameC.update();
-   		 try {
-			Thread.sleep(100);
-		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-   	 }
-   	 /*out.close();
-   	 in.close();
-   	 socket.close();*/
-	}
-    //TODO: write update() that gets the player's current points and the current jackpot from the server
-    //and updates the frame
-    public void update(){
-   	 out.println("update");
-
-   	 //next line printed by server is current jackpot
-   	 try {
-
-   		 jackpot.setText(in.readLine());
-   	 } catch (IOException e) {
-   		 e.printStackTrace();
-   	 }
-   	 try {
-   		 points.setText(in.readLine());
-   	 } catch (IOException e) {
-   		 e.printStackTrace();
-   	 }
+    catch (IOException e) {
+      //pass
     }
-    @Override
-    public void actionPerformed(ActionEvent e) {
-   	 if (e.getSource()==reapButton){
-   		 System.out.println ("clicked the reap button");
-   		 out.println("reap");
-   	 }
-   	 //handle window closing
-   	 
-    }
+  }
+  
+  public static final void setGame(GameThread g){
+    game = g;
+  }
 
-    
+  public void run(){
+    try {
+      String message;
+      while ((message = in.readLine()) != null){
+    	if (message.equals("reap")) {
+    		game.reap(id);
+    	}
+    	else if (message.equals("update")) {
+    		out.println("your points: "+ game.getScore(id));
+    		out.println ("current jackpot: "+game.getJackpot());
+    	}
+        
+      }
+      game.quitPlayer(id);
+      client.close();
+    }
+    catch (IOException e) {
+      //pass
+    }
+  }
 }
